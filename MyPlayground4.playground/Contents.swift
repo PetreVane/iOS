7ab39231f20d3bca5f6260f5a8ce4463ecad63c6
Petre Vane <petre.vane@gmail.com>
2018-06-23 21:46:12 +0200

    Playing with arrays

--- MyPlayground4.playground/Contents.swift
+++ MyPlayground4.playground/Contents.swift
@@ -4,3 +4,59 @@ import Cocoa
 
 
 //var str = "Hello, playground"
+
+var someInt = [Int]()  // declaring an empty array which accepts Integers
+someInt.append(326)   // appending an element
+print (someInt)         // printing the content of the array
+type(of: someInt)       // looking at the type of data
+
+var someStrings: [String] = ["test"]  // declaring another array
+someStrings.append("testing")
+print (someStrings)
+someStrings.append("Petre")
+
+//someStrings = []    // removing all the elements from the array
+
+
+if someStrings.isEmpty {
+    print ("Empty array")
+} else {
+   print ("Array not empty")
+}
+for each in 1...9 {
+    someStrings.append("randomElement")
+    for each in someStrings {
+        someStrings.append(each)
+    }
+    print (someStrings)
+}
+
+print (someStrings.endIndex)
+
+
+
+var something = someStrings
+type(of: something)
+print (something)
+
+something.append("another")
+print (something)
+
+
+
+var threeDoubles = Array(repeating: 10, count: 3) // creating an array with default value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
