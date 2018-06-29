9b3abcff42911f9926d84aca8aae2557cbcf4431
Petre Vane <petre.vane@gmail.com>
2018-06-29 14:59:50 +0200

    Transforming a function into a closure

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -46,11 +46,71 @@ func compareBooks (firstBook: Book, secondBook: Book) -> Bool {
     
 }
 
+/*
 compareBooks(firstBook: book5, secondBook: book3) // this is one way of sorting books
 
-// Another way --> a better way of sorting books is the following:
+ Another way --> a better way of sorting books is the following:
 
 let sortedBooks = allBooks.sorted(by: compareBooks) // Here I am declaring a new constant that holds a copy of the array, sorted using the function which I have earlier declared
 sortedBooks
 
-// Now we have a new array, sorted by
+ Now we have a new array, sorted by reading age
+
+ Now, le't try a closure.
+ First, I am going to copy the function bellow, and comment out the call of the function above
+*/
+
+func compareBooks (firstBook: Book, secondBook: Book) -> Bool {
+    
+    if firstBook.readingAge <= secondBook.readingAge {
+        //print ("First book reading age is smaller or equal than second book reading age")
+        return true
+    } else {
+        //print ("Second book reading age is smaller than first book reading age.")
+        return false
+    }
+    
+}
+
+/*
+ 
+ Step one: remove the function name and the keyword "func": func compareBooks
+ Step two: move the parameters and return type, inside the {}
+ Step three: separate the function parameters and return type, from the function code execution, by inserting the keyword "in":  { (firstBook: Book, secondBook: Book) -> Bool in ...}
+
+*/
+
+{ (firstBook: Book, secondBook: Book) -> Bool
+    in
+    if firstBook.readingAge <= secondBook.readingAge {
+        //print ("First book reading age is smaller or equal than second book reading age")
+        return true
+    } else {
+        //print ("Second book reading age is smaller than first book reading age.")
+        return false
+    }
+    
+}
+
+/* Now, swift is complaining that, the Closure is unused. Written like this, the closure isn't doing anything
+
+The point of writing a closure, is that you want to pass it.
+ So what you need to do is, to cut the entire code between the { } and paste it into the field, where you were calling the initial sorting function:
+ 
+ let sortedBooks = allBooks.sorted(by: { (firstBook: Book, secondBook: Book) -> Bool
+ in
+ if firstBook.readingAge <= secondBook.readingAge {
+ return true
+ } else {
+ return false
+ }
+ 
+ })
+ 
+ This closure is now the argument, into the sorted(by ..) method!
+ 
+ 
+ 
+ 
+*/
+
