1ad5ef85b1845b127efb8ab398389655bbedf96f
Petre Vane <petre.vane@gmail.com>
2018-07-01 15:08:47 +0200

    Differences between classes and structs

--- Classes-Structs.playground/Contents.swift
+++ Classes-Structs.playground/Contents.swift
@@ -39,6 +39,36 @@ class Cars {
     
 }
 
+/*
+One major difference between structs and classes, is that classes support inheritance. Structs do not!
+And perhaps the most important difference between structs and classes: structs are Value Types, while classes are Reference Types.
+ 
+*/
+
+// Example:
+// Declaring a class that holds a message
+class Message {                                        // change to class or structs
+    var internalText: String = "This is a message"
+}
+
+// creating an instance of the class /struct
+var message2 = Message()
+
+// Now, assign the instance to another variable
+var anotherMessage = message2
+anotherMessage.internalText += " ...with some additional text"
+print (message2.internalText)
+print(anotherMessage.internalText)
+
+/*
+ Because classes are reference types, any changes that take place on a instance of the class, will be passed to variables
+ ( and constants) pointing to that instance. And vice-versa.
+ 
+ Now change the keyword "class" to "struct" and run the code again. See that the changes performed on "anotherMessage" will
+ not be passed to the initial instance of the class "message2". That's because structs are Value types and a copy of the "message2"
+ is assigned to "anotherMessage" and not a pointer to the initial instance.
+ */
+
 
 
 
